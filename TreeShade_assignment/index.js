c655c816c33ffada1537592325884215532b2b30
Bhumika07092001 <109783089+Bhumika07092001@users.noreply.github.com>
2024-04-24 14:48:55 +0530

    Add files via upload

---
+++ TreeShade_assignment/index.js
@@ -0,0 +1,22 @@
+function formatText(command, value = null) {
+  document.execCommand(command, false, value);
+}
+function handleDownload() {
+  const leftMargin = document.getElementById("left-margin").value;
+  const topMargin = document.getElementById("top-margin").value;
+  const rightMargin = document.getElementById("right-margin").value;
+  const bottomMargin = document.getElementById("bottom-margin").value;
+
+  console.log("Left Margin:", leftMargin);
+  console.log("Top Margin:", topMargin);
+  console.log("Right Margin:", rightMargin);
+  console.log("Bottom Margin:", bottomMargin);
+}
+function generatePDF() {
+  const doc = new jsPDF();
+  const text = document.getElementById("editor").innerHTML;
+
+  doc.setMargins(10, 10, 10, 10);
+  doc.fromHTML(text, 15, 15);
+  doc.save("formatted_text.pdf");
+}
